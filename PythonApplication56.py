0a217178b1416f0540516686f3e858e5885b0906
SEYEON_PARK <fourleaf123@naver.com>
2021-03-07 02:15:54 +0900

    Python함수 while문 연습!(3)

---
+++ PythonApplication56.py
@@ -0,0 +1,16 @@
+print("몇 번째 소수까지 더해야 100보다 큰 수가 될까요?")
+
+sum=2
+a=3
+num=1
+
+while sum<=100:
+    for i in range(2,a):
+      if (a%i==0):
+          a+=1
+
+    sum+=a
+    a+=1
+    num+=1
+
+print("정답은 ",num,"번째입니다!", sep="")
